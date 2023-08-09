9a2d7efbac4056cbaaad987d9621156b6a53d4f7
laura Mesa <lauramesa71@gmail.com>
2023-08-09 00:21:53 -0500

    conexion BD, consulta y visualizacion en un JFrame

--- src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -1,7 +1,10 @@
 package com.alura.jdbc.controller;
 
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductoController {
 
@@ -13,9 +16,33 @@ public class ProductoController {
 		// TODO
 	}
 
-	public List<?> listar() {
-		// TODO
-		return new ArrayList<>();
+	public List<Map<String, String>> listar() {
+		String url = "jdbc:mysql://localhost/control_stock?useTimeZone=true&serverTimeZone=UTC";
+		String user = "root";
+		String password = "root12345";
+
+		ArrayList<Map<String, String>> resultado = null;
+		try (Connection con = DriverManager.getConnection(url, user, password)) {
+			Statement statement = con.createStatement();
+			statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+
+			ResultSet resultSet = statement.getResultSet();
+			resultado = new ArrayList<>();
+			while (resultSet.next()) {
+				Map<String, String> fila = new HashMap<>();
+				//resultSet.getInt("ID");
+				fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+				fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+				fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+				fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+
+				resultado.add(fila);
+			}
+		} catch (SQLException e) {
+			System.err.println("error " + e.getMessage());
+		}
+		// return new ArrayList<>();
+		return resultado;
 	}
 
     public void guardar(Object producto) {
--- src/main/java/com/alura/jdbc/pruebas/PruebaConexion.java
+++ src/main/java/com/alura/jdbc/pruebas/PruebaConexion.java
@@ -8,9 +8,9 @@ public class PruebaConexion {
 
     public static void main(String[] args) throws SQLException {
         Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+                "jdbc:mysql://localhost/control_stock?useTimeZone=true&serverTimeZone=UTC",
                 "root",
-                "root1234");
+                "root12345");
 
         System.out.println("Cerrando la conexión");
 
