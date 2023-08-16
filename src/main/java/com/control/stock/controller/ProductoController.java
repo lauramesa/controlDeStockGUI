8f427aa1eb885291397513729d91ee62a64d698f
laura Mesa <lauramesa71@gmail.com>
2023-08-16 02:31:31 -0500

    evitar inyecciones SQL

--- src/main/java/com/control/stock/controller/ProductoController.java
+++ src/main/java/com/control/stock/controller/ProductoController.java
@@ -14,13 +14,18 @@ public class ProductoController {
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection con = factory.recuperaConexion();
 
-		Statement statement = con.createStatement();
+		PreparedStatement statement = con
+				.prepareStatement("UPDATE PRODUCTO SET "
+						+ " NOMBRE = ?, "
+						+ " DESCRIPCION = ?,"
+						+ " CANTIDAD = ?"
+						+ " WHERE ID = ?");
 
-		statement.execute("UPDATE PRODUCTO SET "
-				+ " NOMBRE = '" + nombre + "'"
-				+ ", DESCRIPCION = '" + descripcion + "'"
-				+ ", CANTIDAD = " + cantidad
-				+ " WHERE ID = " + id);
+		statement.setString(1, nombre);
+		statement.setString(2, descripcion);
+		statement.setInt(3, cantidad);
+		statement.setInt(4, id);
+		statement.execute();
 
 		int updateCount = statement.getUpdateCount();
 
@@ -33,11 +38,14 @@ public class ProductoController {
 		try {
 			Connection  con = new ConnectionFactory().recuperaConexion();
 
-			Statement statement = con.createStatement();
+			PreparedStatement statement = con
+					.prepareStatement("DELETE FROM PRODUCTO WHERE ID = ?");
+			statement.setInt(1, id);
+			statement.execute();
 
-			statement.execute("DELETE FROM PRODUCTO WHERE ID = " +id);
+			int updateCount = statement.getUpdateCount();
 
-			return statement.getUpdateCount();
+			return updateCount;
 
 		} catch (SQLException e){
 			throw new RuntimeException(e);
@@ -46,18 +54,22 @@ public class ProductoController {
 
 	public List<Map<String, String>> listar() {
 
-		ArrayList<Map<String, String>> resultado = null;
+		List<Map<String, String>> resultado = new ArrayList<>();
 
 		try {
-			Connection con = new ConnectionFactory().recuperaConexion();
-			Statement statement = con.createStatement();
-			statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+			ConnectionFactory factory = new ConnectionFactory();
+			Connection con = factory.recuperaConexion();
+
+			PreparedStatement statement = con
+					.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+			statement.execute();
 
 			ResultSet resultSet = statement.getResultSet();
-			resultado = new ArrayList<>();
+
+
+
 			while (resultSet.next()) {
 				Map<String, String> fila = new HashMap<>();
-				//resultSet.getInt("ID");
 				fila.put("ID", String.valueOf(resultSet.getInt("ID")));
 				fila.put("NOMBRE", resultSet.getString("NOMBRE"));
 				fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
@@ -65,8 +77,9 @@ public class ProductoController {
 
 				resultado.add(fila);
 			}
+
 		} catch (SQLException e) {
-			System.err.println("error " + e.getMessage());
+			throw new RuntimeException(e);
 		}
 		// return new ArrayList<>();
 		return resultado;
@@ -76,11 +89,13 @@ public class ProductoController {
 		try{
 			Connection con = new ConnectionFactory().recuperaConexion();
 
-			Statement statement = con.createStatement();
-			statement.execute("INSERT INTO PRODUCTO(nombre,descripcion,cantidad)"
-					+"VALUES('" +producto.get("NOMBRE") + "', '"
-					+producto.get("DESCRIPCION")+ "', "
-					+producto.get("CANTIDAD")+")", Statement.RETURN_GENERATED_KEYS );
+			PreparedStatement statement = con.prepareStatement("INSERT INTO PRODUCTO(nombre,descripcion,cantidad)" +
+					"VALUES(?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			statement.setString(1, producto.get("NOMBRE"));
+			statement.setString(2, producto.get("DESCRIPCION"));
+			statement.setInt(3, Integer.parseInt(producto.get("CANTIDAD")));
+
+			statement.execute();
 
 			ResultSet resultSet = statement.getGeneratedKeys();
 
